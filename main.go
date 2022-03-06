dae972a80348a9669eae43424ce8056a389044a7
ArkaBarua173 <40537823+Arka-Barua@users.noreply.github.com>
2022-03-06 21:08:04 +0600

    user routes and theirs response created

--- main.go
+++ main.go
@@ -4,14 +4,26 @@ import (
 	"log"
 
 	"example.com/go-rest-api/database"
+	"example.com/go-rest-api/routes"
 	"github.com/gofiber/fiber/v2"
 )
+
+func setupRoutes(app *fiber.App) {
+	// user endpoints
+	app.Post("/api/users", routes.CreateUser)
+	app.Get("/api/users", routes.GetUsers)
+	app.Get("api/users/:id", routes.GetUser)
+	app.Put("api/users/:id", routes.UpdateUser)
+	app.Delete("api/users/:id", routes.DeleteUser)
+}
 
 func main() {
 
 	database.ConnectDb()
 
 	app := fiber.New()
+
+	setupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 
